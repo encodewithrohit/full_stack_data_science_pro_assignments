3f1666a54ce0c1469930efd80ec5248287f29d09
Rohit Kumar Gupta <151347543+Encode-With-Rohit@users.noreply.github.com>
2024-06-07 20:32:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def helloworld() :
-    return "Hello World!"
+def home():
+    return render_template('home.html')
 
-if __name__ == '__main__' :
-    app.run(port=5005)
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
